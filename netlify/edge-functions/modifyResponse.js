911acf961493324b204695f5dfd8c4fd3f799ec7
Harsh Patel <harshpatel131090@gmail.com>
2022-09-03 21:50:49 -0500

    allow origin from requet in netlify edge function

--- netlify/edge-functions/modifyResponse.js
+++ netlify/edge-functions/modifyResponse.js
@@ -1,9 +1,12 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (request, context) => {
+  const requestOrigin = request.headers.referer
   console.log('**** EDGE FUNC LOGS ****')
   console.log(request)
+  console.log(requestOrigin)
   console.log('**** EDGE FUNC LOGS ****')
+  
   const response = await context.next();
-  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Origin", requestOrigin);
   return response;
 };
