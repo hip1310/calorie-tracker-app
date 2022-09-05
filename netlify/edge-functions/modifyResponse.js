7a75f345bb2014c0fe1ed1fb8057f00014d66f7f
Harsh Patel <harshpatel131090@gmail.com>
2022-09-05 14:50:34 -0500

    fix netlify edge func use domain origin

--- netlify/edge-functions/modifyResponse.js
+++ netlify/edge-functions/modifyResponse.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (request, context) => {
   const domain = (new URL(request.url))
-  const requestOrigin = domain.protocol + '//' + domain.hostname + '/'
+  const requestOrigin = domain.origin
   console.log('**** EDGE FUNC LOGS ****')
   console.log(request)
   console.log(requestOrigin)
   console.log('**** EDGE FUNC LOGS ****')
   
   const response = await context.next();
-  response.headers.set("Access-Control-Allow-Origin", "https://www.test.com");
+  response.headers.set("Access-Control-Allow-Origin", requestOrigin);
   return response;
 };
